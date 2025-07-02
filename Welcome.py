c5b9442e7eec2e550e4154c1e2c00edb84fefa3f
lvbauer <122317739+lvbauer@users.noreply.github.com>
2025-07-01 23:53:53 -0400

    fixed 'fiduciary':   Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -8,7 +8,7 @@ def main():
 
     st.markdown("### About")
     st.markdown("Web-Glyphics is a simple online sandbox for testing fiduciary markers in images written by Lucas Bauer using [OpenCV](opencv.org) and [Streamlit](https://streamlit.io/)."
-    " I am currently developing applications for using fiduciary markers in life sciences image acquisition and analysis.")
+    " I am currently developing applications for using fiducial markers in life sciences image acquisition and analysis.")
 
     st.markdown("Contact me at: lucas.bauer848@gmail.com")
     st.markdown("My Github: [lvbauer](https://github.com/lvbauer)")
